1a4ca64c24c3926119ae6f83634f546e9bddcba6
Zach Thomas <zwthomas13@gmail.com>
2020-05-10 21:14:22 -0500

    starting ui

---
+++ stream-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import VideoPlayer from "./components/VideoPlayer"
+
+function App() {
+  return (
+    <div className="App">
+      <VideoPlayer />
+    </div>
+  );
+}
+
+export default App;
